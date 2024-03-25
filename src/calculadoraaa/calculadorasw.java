5003ba469c7699d0d55743aa17d70d120b95d420
anika19sc <anacesiasantacruz@gmail.com>
2024-03-25 13:00:28 -0300

    hola

---
+++ src/calculadoraaa/calculadorasw.java
@@ -0,0 +1,14 @@
+package calculadoraaa;
+
+public class calculadorasw {
+        public static void main(String[] args){
+
+            int numero;
+            Scanner leerConsola = new Scanner(System.in);
+            System.out.print("Introduzca un numero: ");
+            numero = leerConsola.nextInt();
+            System.out.println("Su numero es "+ numero);
+
+        }
+    }
+
